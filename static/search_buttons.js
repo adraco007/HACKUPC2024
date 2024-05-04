26e06cfde8c5bf46e488d90e6c6b46aaedacb8ae
Miquel Rodríguez Sansaloni <129008896+mrs714@users.noreply.github.com>
2024-05-04 19:30:21 +0200

    mm

---
+++ static/search_buttons.js
@@ -0,0 +1,5 @@
+// Get buttons prevButton and nextButton
+let prevButton = document.getElementById('prevButton');
+let nextButton = document.getElementById('nextButton');
+
+// Add event listeners to the buttons
