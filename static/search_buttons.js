d443b535649729e505a716248a76cc7d6d66b096
Miquel Rodríguez Sansaloni <129008896+mrs714@users.noreply.github.com>
2024-05-05 04:07:28 +0200

    web

--- static/search_buttons.js
+++ static/search_buttons.js
@@ -4,3 +4,26 @@ let nextButton = document.getElementById('nextButton');
 
 // Add event listeners to the buttons
 let urls = [];
+let actual_url = NaN;
+
+// once the person uses the next button:
+nextButton.addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent form submission
+    let index = urls.indexOf(actual_url);
+    if (index < urls.length - 1) {
+        actual_url = urls[index + 1];
+        let rightPhoto = document.getElementById('outfitImage');
+        rightPhoto.src = actual_url;
+    }
+});
+
+// once the person uses the prev button:
+prevButton.addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent form submission
+    let index = urls.indexOf(actual_url);
+    if (index > 0) {
+        actual_url = urls[index - 1];
+        let rightPhoto = document.getElementById('outfitImage');
+        rightPhoto.src = actual_url;
+    }
+});
