b362fb71632d130f45ed6ddc11b511379cd79925
Barrenee <barrenecheapereapablo@gmail.com>
2024-05-05 03:03:44 +0200

    returns embedings and indexes

--- src/processor.py
+++ src/processor.py
@@ -1,9 +1,9 @@
 #from PIL import Image
 import numpy as np
 import random
-from src.model_clip import ClipModel
-from src.embeds_visualizer_class import EmbedsVisualizer
-from src.clusterPrevi import ImageClassifier
+from model_clip import ClipModel
+from embeds_visualizer_class import EmbedsVisualizer
+from clusterPrevi import ImageClassifier
 import pickle
 
 """
@@ -33,6 +33,20 @@ class Processor:
         print(vector_similaridades)
         return vector_indices, vector_similaridades
     
+    def select_images_optimized(self, vector):
+        """
+        given a vector of 1s and 0s, take from the images the ones that have a 1;
+        separate them in clusters. inside each cluster, take the ones that are most similar from differents sets, take the cluster with the highest similarity, and return its top 4 similar images
+        """
+
+        embeddings, indexes = self.get_embeddings(image_vector = vector, image_path="./data/images", created_model=True, model_pathfile="./models/clip_model.pkl")
+
+        e = EmbedsVisualizer()
+        e.get_max_similarity_optimized(embeddings=embeddings, indexes=indexes)
+        #image = Image.open('data/generated_images/image1.png')
+        """print(vector_similaridades)
+        return vector_indices, vector_similaridades"""
+    
     def get_embeddings(self, image_vector, image_path, selected_image_pathfile=None, created_model = False, model_pathfile = "./models/clip_model.pkl"):
         """
         Given a vector of images, return the embeddings of the images
@@ -42,14 +56,21 @@ class Processor:
         else:
             model = ClipModel()
         
-        embeddings = model.process_selected_images(image_vector, images_path=image_path)
+        embeddings, indexes = model.process_selected_images(image_vector, images_path=image_path)
         if selected_image_pathfile:
             selected_image_embedding = model.process_select_image(image_path=selected_image_pathfile)
             return embeddings, selected_image_embedding
         else:
-            return embeddings
+            return embeddings, indexes
         
-#Processor().select_images([0,0,0,0,0,1,1,1,0,0,1,1,0,0,1,0,0,0,0,0,0,1,1,0,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+    def get_canta_vectors(self):
+        """
+        Get the vectors of the canta rero
+        """
+        ImageClassifier().load_data()
+        pass    
+        
+Processor().select_images_optimized([0,0,0,0,0,1,1,1,0,0,1,1,0,0,1,0,0,0,0,0,0,1,1,0,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
 #Processor().find_outfit([0,0,0,0,0,1,1,1,0,0,1,1,0,0,1,0,0,0,0,0,0,1,1,0,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], selected_image_pathfile = "./data/images/img_0_1.jpg")
 
 
